b34ca3c224459c6662c72fd3fc8035c51ea3e41b
Diogo Golovanevsky Monteiro <diogo.gmt@gmail.com>
2012-11-14 02:38:30 -0500

    Updating some notes.

--- profile/md5-optimized.cpp
+++ profile/md5-optimized.cpp
@@ -1,20 +1,32 @@
+
+
 Append run 1:
+md5_append(&state, (const md5_byte_t *)word, strlen(word));
+
 
 const md5_byte_t *p = data;
 int left = nbytes;
-int offset = (pms->count[0] >> 3) & 63;
+int offset = (state.count[0] >> 3) & 63;
 md5_word_t nbits = (md5_word_t)(nbytes << 3);
 
 /* Update the message length. */
-pms->count[1] += nbytes >> 29;
-pms->count[0] += nbits;
+state.count[1] += nbytes >> 29;
+state.count[0] += nbits;
 
 /* Process a final partial block. */
-memcpy(pms->buf, p, left);
+memcpy(state.buf, p, left);
 
 
 
 Append run 2:
+pms = state
+static const md5_byte_t pad[64] = {
+0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+md5_append(pms, pad, ((55 - (pms->count[0] >> 3)) & 63) + 1);
 
 
 const md5_byte_t *p = data;
@@ -36,6 +48,14 @@ memcpy(pms->buf + offset, p, copy);
 
 
 Append run 3:
+pms = state
+// data is calculated before the call to append 2
+md5_byte_t data[8];
+for (i = 0; i < 8; ++i) {
+	data[i] = (md5_byte_t)(pms->count[i >> 2] >> ((i & 3) << 3));
+}
+md5_append(pms, data, 8);
+
 
 
 const md5_byte_t *p = data;
